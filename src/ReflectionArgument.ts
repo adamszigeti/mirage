ffdad82fe58257083ac95b16a6d20023064c5dc2
adamszigeti <adam.szigeti@outlook.com>
2017-03-04 12:54:38 +0100

    Reindented code, and fixed RArgument's syntax coloring

--- src/ReflectionArgument.ts
+++ src/ReflectionArgument.ts
@@ -5,129 +5,129 @@ import {ReflectionMethod} from "./ReflectionMethod";
  */
 export class ReflectionArgument
 {
-    /**
-     * The definition of the argument, as it is written in the source code.
-     */
-    protected argument: {name: string, hasDefaultValue: boolean, value: any};
+  /**
+   * The definition of the argument, as it is written in the source code.
+   */
+  protected argument: {name: string, hasDefaultValue: boolean, value: any};
 
-    /**
-     * ReflectionArgument constructor.
-     * 
-     * @prop string The argument itself, as it is written in the source code.
-     */
-    constructor(argument: string)
-    {
-        const parsed = argument.split("=");
-        this.argument = {
-            name: parsed[0].trim(),
-            value: this.sanitize(parsed[1]),
-            hasDefaultValue: 1 < parsed.length,
-        };
-    }
+  /**
+   * ReflectionArgument constructor.
+   * 
+   * @param  {String}  argument  The argument itself, as in the source.
+   */
+  constructor(argument: string)
+  {
+    const parsed: string[] = argument.split("=");
+    this.argument = {
+      name: this.trimSpaces(parsed[0]),
+      value: this.sanitize(parsed[1]),
+      hasDefaultValue: 1 < parsed.length
+    };
+  }
 
-    /**
-     * Tells this argument's name.
-     * 
-     * @returns string
-     */
-    public name() : string
-    {
-        return this.argument.name;
-    }
+  /**
+   * Tells this argument's name.
+   * 
+   * @return {String}
+   */
+  public name() : string
+  {
+    return this.argument.name;
+  }
 
-    /**
-     * Tells if this argument has a default value or not.
-     * 
-     * @returns boolean
-     */
-    public hasDefaultValue() : boolean
-    {
-        return !! this.argument.hasDefaultValue;
-    }
+  /**
+   * Tells if this argument has a default value or not.
+   * 
+   * @return {boolean}
+   */
+  public hasDefaultValue() : boolean
+  {
+    return !! this.argument.hasDefaultValue;
+  }
 
-    /**
-     * Returns the default value of the argument.
-     * 
-     * @returns any
-     */
-    public defaultValue() : any
-    {
-        let value = this.argument.value;
-        if (value = this.trimQuotes(value))
-            return this.isAnObject(value) ? eval(`(${value})`) : value;
+  /**
+   * Returns the default value of the argument.
+   * 
+   * @return {any}
+   */
+  public defaultValue() : any
+  {
+    let value = this.argument.value;
+    if (value = this.trimQuotes(value))
+      return this.isAnObject(value) ? eval(`(${value})`) : value;
 
+    return value;
+  }
+
+  /**
+   * Transforms the string representations of "undefined" and "null" (the 
+   * general empty values) to their respective, typesafe forms. If the value 
+   * is anything but these two types, it will return it without modification.
+   * 
+   * @param  {String|undefined}  value  The value to sanitize.
+   * 
+   * @return {undefined|null|any}
+   */
+  protected sanitize(value: string | undefined) : undefined | null | any
+  {
+    switch (value = this.trimSpaces(value)) {
+      case "undefined":
+        return undefined;
+
+      case "null":
+        return null;
+
+      default:
         return value;
     }
+  }
 
-    /**
-     * Transforms the string representations of "undefined" and "null" (the 
-     * general empty values) to their respective, typesafe forms. If the value 
-     * is anything but these two types, it will return it without modification.
-     * 
-     * @param string|undefined value The value to sanitize.
-     * 
-     * @return undefined|null|any
-     */
-    protected sanitize(value: string | undefined) : undefined | null | any
-    {
-        switch (value = this.trimSpaces(value)) {
-            case "undefined":
-                return undefined;
-            
-            case "null":
-                return null;
-            
-            default:
-                return value;
-        }
-    }
+  /**
+   * Checks if the given value is undefined, or null. Since it is possible 
+   * that these values were declared explicitly, this also checks if these 
+   * values are present as strings.
+   * 
+   * @return {Boolean}
+   */
+  protected isNullOrUndefined(value: string) : boolean
+  {
+    return ! value || value === "undefined" || value === "null";
+  }
 
-    /**
-     * Checks if the given value is undefined, or null. Since it is possible 
-     * that these values were declared explicitly, this also checks if these 
-     * values are present as strings.
-     * 
-     * @returns boolean
-     */
-    protected isNullOrUndefined(value: string) : boolean
-    {
-        return ! value || value === "undefined" || value === "null";
-    }
+  /**
+   * Checks if the passed (default) value is an object or not.
+   * 
+   * @return {Boolean}
+   */
+  protected isAnObject(value: string) : boolean
+  {
+    return value.startsWith('{');
+  }
 
-    /**
-     * Checks if the passed (default) value is an object or not.
-     * 
-     * @returns boolean
-     */
-    protected isAnObject(value: string) : boolean
-    {
-        return value.startsWith('{');
-    }
+  /**
+   * Trims the given value if it is not null or undefined. When it is, it 
+   * returns any of those two, unprocessable values.
+   * 
+   * @param  {String}  value  The value to sanitize.
+   * 
+   * @return {String}  The sanitized string, or the unprcessable primitives.
+   */
+  protected trimSpaces(value: string) : string
+  {
+    return value ? value.trim() : value;
+  }
 
-    /**
-     * Trims the given value if it is not null or undefined. When it is, it 
-     * returns any of those two, unprocessable values.
-     * 
-     * @param string value The value to sanitize.
-     * 
-     * @returns string The sanitized string, or the unprcessable primitives.
-     */
-    protected trimSpaces(value: string) : string
-    {
-        return value ? value.trim() : value;
-    }
-
-    /**
-     * Removes the double- and single quotes from the beginning and the end of 
-     * the given string. If the passed value is undefined or null, it will 
-     * silently just return them without modification.
-     * 
-     * @param string value The value to be trimmed.
-     * 
-     * @returns string|undefined|null
-     */
-    protected trimQuotes(value: string) : string | undefined | null
-    {
-        return value ? value.replace(/^["']/, "").replace(/["']$/, "") : value;
-    }
+  /**
+   * Removes the double- and single quotes from the beginning and the end of 
+   * the given string. If the passed value is undefined or null, it will 
+   * silently just return them without modification.
+   * 
+   * @param  {String}  value  The value to be trimmed.
+   * 
+   * @return  {String|undefined|null}
+   */
+  protected trimQuotes(value: string) : string | undefined | null
+  {
+    return value ? value.replace(/^["']/, "").replace(/["']$/, "") : value;
+  }
 }
--- src/ReflectionClass.ts
+++ src/ReflectionClass.ts
@@ -2,95 +2,95 @@ import {ReflectionMethod} from "./ReflectionMethod";
 
 export class ReflectionClass
 {
-    protected type;
-    protected subject: Object;
-    protected constructorMethod: ReflectionMethod;
+  protected type;
+  protected subject: Object;
+  protected constructorMethod: ReflectionMethod;
 
-    public constructor(subject: any)
-    {
-        this.type = subject;
-        this.subject = subject instanceof Function ? new subject : subject;
-        this.constructorMethod = new ReflectionMethod(this.subject, "constructor");
+  public constructor(subject: any)
+  {
+    this.type = subject;
+    this.subject = subject instanceof Function ? new subject : subject;
+    this.constructorMethod = new ReflectionMethod(this.subject, "constructor");
+  }
+
+  /**
+   * Creates the subject with the passed arguments.
+   * 
+   * @returns Object
+   */
+  public construct(...args) : Object
+  {
+    if (this.type instanceof Function)
+      return this.constructorMethod.invoke(...args);
+
+    throw new Error("Should not try to (re)construct an instance which already exists!");
+  }
+
+  /**
+   * Lists all the methods in the subject class.
+   * 
+   * @returns ReflectionMethod[]
+   */
+  public methods() : ReflectionMethod[]
+  {
+    let methods = [];
+
+    this.prototypes().forEach((prototype: Object) => {
+      let object = new ReflectionClass(prototype);
+      methods = methods.concat(object.members().filter((member: string) => {
+        return prototype[member] instanceof Function;
+      }));
+    });
+
+    return [...new Set(methods)].map(
+      method => new ReflectionMethod(this.subject, method)
+    );
+  }
+
+  /**
+   * Lists all the properties of the subject class, without the methods.
+   * 
+   * @returns string[]
+   */
+  public properties() : string[]
+  {
+    return Object.getOwnPropertyNames(this.subject).filter((property: string) => {
+      return ! (this.subject[property] instanceof Function);
+    });
+  }
+
+  /**
+   * Lists the properties, methods, and symbols in the subject class as an 
+   * array of strings.
+   * 
+   * @returns string[]
+   */
+  public members() : string[]
+  {
+    let propsByProto = this.prototypes().map(prototype => {
+      return Object.getOwnPropertyNames(prototype);
+    });
+
+    let allProps = propsByProto.reduce((prevProtoProps, nextProtoProps) => {
+      return prevProtoProps.concat(nextProtoProps);
+    });
+
+    return [...new Set(allProps)];
+  }
+
+  /**
+   * Lists all the prototypes of the subject, except for the Object prototype.
+   * 
+   * @returns Object[]
+   */
+  public prototypes() : Object[]
+  {
+    let prototype = this.subject;
+    let collection = [prototype];
+    while ((prototype = Object.getPrototypeOf(prototype)) && Object.getPrototypeOf(prototype)) {
+      collection.push(prototype);
     }
 
-    /**
-     * Creates the subject with the passed arguments.
-     * 
-     * @returns Object
-     */
-    public construct(...args) : Object
-    {
-        if (this.type instanceof Function)
-            return this.constructorMethod.invoke(...args);
-
-        throw new Error("Should not try to (re)construct an instance which already exists!");
-    }
-
-    /**
-     * Lists all the methods in the subject class.
-     * 
-     * @returns ReflectionMethod[]
-     */
-    public methods() : ReflectionMethod[]
-    {
-        let methods = [];
-        
-        this.prototypes().forEach((prototype: Object) => {
-            let object = new ReflectionClass(prototype);
-            methods = methods.concat(object.members().filter((member: string) => {
-                return prototype[member] instanceof Function;
-            }));
-        });
-
-        return [...new Set(methods)].map(
-            method => new ReflectionMethod(this.subject, method)
-        );
-    }
-
-    /**
-     * Lists all the properties of the subject class, without the methods.
-     * 
-     * @returns string[]
-     */
-    public properties() : string[]
-    {
-        return Object.getOwnPropertyNames(this.subject).filter((property: string) => {
-            return ! (this.subject[property] instanceof Function);
-        });
-    }
-
-    /**
-     * Lists the properties, methods, and symbols in the subject class as an 
-     * array of strings.
-     * 
-     * @returns string[]
-     */
-    public members() : string[]
-    {
-        let propsByProto = this.prototypes().map(prototype => {
-            return Object.getOwnPropertyNames(prototype);
-        });
-
-        let allProps = propsByProto.reduce((prevProtoProps, nextProtoProps) => {
-            return prevProtoProps.concat(nextProtoProps);
-        });
-
-        return [...new Set(allProps)];
-    }
-
-    /**
-     * Lists all the prototypes of the subject, except for the Object prototype.
-     * 
-     * @returns Object[]
-     */
-    public prototypes() : Object[]
-    {
-        let prototype = this.subject;
-        let collection = [prototype];
-        while ((prototype = Object.getPrototypeOf(prototype)) && Object.getPrototypeOf(prototype)) {
-            collection.push(prototype);
-        }
-
-        return collection;
-    }
+    return collection;
+  }
 }
